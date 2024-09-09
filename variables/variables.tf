e206c30959219e658048350aa065762eb3700ed8
Amit Mehta <amitmehta.aws@gmail.com>
2024-09-10 01:56:22 +0530

    adding context in variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,10 +1,15 @@
+# Variable definition can contain default value and other attributes like description and type
 variable "static_ip" {
   description = "Jump server's IP"
   default = "10.16.0.0/32"
+  type = string
 }
 
 variable "ssh_port" {
     default = 22
+    type = number
 }
 
-variable "http_port" {}
+variable "http_port" {
+    type = number
+}
