6a04b8c595736dab16803970c5087efb794dc910
Amit Mehta <amitmehta.aws@gmail.com>
2024-09-11 15:56:41 +0530

    Adding README update and Datasource file.

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - `terraform destroy -target aws_instance.myec2`: Destroy specific resource.
 - `terraform console`: Interactive command line utility to test functions.
 - `terraform refresh`: Fetches the current state of infrastructure and update the state file.
+- `terraform fmt`: Format the Terraform code as per the standards.
--- datasource/aws-datasource.tf
+++ datasource/aws-datasource.tf
@@ -1,13 +1,18 @@
 provider "aws" {}
 
 # Reading data from AWS at runtime using Datasource
-data "aws_ami" "amazon_linux" {
-
+data "aws_ami" "ubuntu" {
   most_recent = true
+  owners = [ "amazon" ]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
 
 # Using data from datasource in resource provisioning
 resource "aws_instance" "ec2-instance" {
-  ami = data.aws_ami.amazon_linux.image_id
+  ami = data.aws_ami.ubuntu.image_id
   instance_type = "t2.micro"
 }
